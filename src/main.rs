aa29d7170fcfd1931c1c4fb76c65106347e64fc8
blackhawkpro23 <chandler.harrison@outlook.com>
2023-10-09 20:15:18 -0500

    added toml config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+mod model;
+
+use crate::model::EventModel;
+use serde_derive::Deserialize;
+use std::fs;
+use std::process::exit;
+use toml;
+
+const CONFIGFILE: &str = "config.toml";
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    path: Vec<Path>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Path {
+    filepath: String,
+}
+
 fn main() {
     println!("Hello, world!!");
+
+    let m = EventModel {
+        filepath: "file.exe".to_string(),
+        time: 1234,
+    };
+
+    println!("check out my model {:?}", m);
+
+    let cfg = match fs::read_to_string(CONFIGFILE) {
+        Ok(c) => c,
+        Err(_) => {
+            // Write `msg` to `stderr`.
+            eprintln!("Could not read file `{}`", CONFIGFILE);
+            exit(1);
+        }
+    };
+
+    let config: Config = match toml::from_str(&cfg) {
+        Ok(d) => d,
+        Err(_) => {
+            // Write `msg` to `stderr`.
+            eprintln!("Unable to load data from `{}`", CONFIGFILE);
+            // Exit the program with exit code `1`.
+            exit(1);
+        }
+    };
+
+    for path in config.path {
+        println!("path {:?}", path.filepath)
+    }
 }
---
+++ src/model.rs
@@ -0,0 +1,5 @@
+#[derive(Debug)]
+pub struct EventModel {
+    pub filepath: String,
+    pub time: u64,
+}
